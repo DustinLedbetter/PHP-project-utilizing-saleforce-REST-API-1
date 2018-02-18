40ea687c810b14cb571641113268853cdfc6fc4e
Dustin Ledbetter <dustinledbetter9@gmail.com>
2018-02-18 18:11:53 -0500

    Add files via upload

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+	define("CLIENT_ID", "consumer key");
+
+	define("CLIENT_SECRET", "consumer key");
+
+	define("REDIRECT_URI", "https://localhost/resttest/oauth_callback.php");
+
+	define("LOGIN_URI", "https://login.salesforce.com");
+
+?>
